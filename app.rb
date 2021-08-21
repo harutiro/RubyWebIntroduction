e004dd3d851e83d3758a2d6dd16950cc91b71e09
EC2 Default User <ec2-user@ip-10-1-12-21.ap-northeast-1.compute.internal>
2021-08-21 06:25:10 +0000

    フォームとappを結びつける部分完成

--- app.rb
+++ app.rb
@@ -14,3 +14,30 @@ get '/' do
     @categories = Category.all
     erb :index
 end
+
+post '/create' do
+    # img_url = ''
+    # if params[:file]
+    #     img = params[:file]
+    #     tempfile = img[:tempfile]
+    #     upload = Cloudinary::Uploader.upload(tempfile.path)
+    #     img_url = upload['url']
+    # end
+    
+    Contribution.create({
+        main_title: params[:main_title],
+        sub_title: params[:sub_title],
+        user_name: params[:user_name],
+        winos: params[:windows],
+        macos: params[:macos],
+        linuxos: params[:linux],
+        message: params[:message],
+        url: params[:url],
+        like: 0,
+        category_id: params[:category]
+    })
+    
+    redirect '/'
+end
+
+
